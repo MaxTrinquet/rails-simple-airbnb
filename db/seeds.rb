da3a1fa0ba83c81961f568fe602b699df81e94ea
Maxime Trinquet <maxime.trinquet@gmail.com>
2020-03-11 19:03:45 +0800

    card finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 rue des lauries Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 199,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Appartement Paris',
+  address: 'Sentier 75009 Paris',
+  description: 'Superbe appartement a Paris meem avec une large kitchen and a beautiful conservatory',
+  price_per_night: 544,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: 'Harry Potter and friends',
+  address: '3/4 Poudlard',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 35,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Starwars theme',
+  address: '10 rue de la vega Paris',
+  description: 'Lorem ipsum dolor sit amet.n living area, large kitchen and a beautiful conservatory',
+  price_per_night: 434,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, debitis.A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 45,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+  address: 'Lorem ipsum dolor sit amet.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat hic iusto dolore at. Placeat reprehenderit illum quis provident quidem ab molestiae suscipit possimus, dolores soluta minus temporibus molestias sapiente? Quo omnis veniam provident deleniti! Beatae dignissimos, ipsa mollitia illum dolorem.ry',
+  price_per_night: 75,
+  number_of_guests: 3
+)
